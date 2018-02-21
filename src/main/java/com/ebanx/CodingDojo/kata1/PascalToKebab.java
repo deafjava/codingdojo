1847835d565cd706d7e3c8255c93154a909bf01c
Caio Cascaes <caio.cascaes@ebanx.com>
2018-02-21 15:45:21 -0300

    Snapshot of Coding Dojo day 21 of February
     - And the suggested solution.

--- src/main/java/com/ebanx/CodingDojo/kata1/PascalToKebab.java
+++ src/main/java/com/ebanx/CodingDojo/kata1/PascalToKebab.java
@@ -1,5 +1,37 @@
 package com.ebanx.CodingDojo.kata1;
 
-public class PascalToKebab {
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
+public class PascalToKebab {
+    public static String convert(String inputString) {
+
+        String[] splittedWords = inputString.split("(?=[A-W])");
+        return Arrays.stream(splittedWords).map(String::toLowerCase).collect(Collectors.joining("-"));
+//
+//        StringBuffer outputString = new StringBuffer();
+//
+//        String newString = inputString.replaceAll("([A-Z])", "-$*");
+//
+//        for (int i = 0; i < inputString.length(); ++i) {
+//            char currentChar = inputString.charAt(i);
+//
+//            String value = String.valueOf(currentChar);
+//
+//            if (value.equals(value.toUpperCase())) {
+//                if (i > 0) {
+//                    outputString.append('-');
+//                }
+//                outputString.append(convertToLowercase(currentChar));
+//            } else {
+//                outputString.append(currentChar);
+//            }
+//        }
+//
+//        return outputString.toString();
+    }
+
+//    public static Character convertToLowercase(Character c) {
+//        return String.valueOf(c).toLowerCase().charAt(0);
+//    }
 }
